1827a92425ea0e4aa881a4b2ab322a1472d66231
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-09-26 17:12:44 -0600

    create proxy for logger to allow different loggers to be used

--- src/supasaas/__init__.py
+++ src/supasaas/__init__.py
@@ -1,2 +1,32 @@
-def hello() -> str:
-    return "Hello from supasaas!"
+import logging
+from typing import Any, Callable
+
+
+class LoggerProxy:
+    def __init__(self):
+        self._logger = None
+
+    def set_logger(self, logger: Any) -> None:
+        self._logger = logger
+
+    def _add_basic_logger(self) -> None:
+        self._logger = logging.getLogger("supasaas")
+        self._logger.basicConfig()
+
+    def __getattr__(self, name: str) -> Callable:
+        if self._logger is None:
+            self._add_basic_logger()
+        return getattr(self._logger, name)
+
+
+logger = LoggerProxy()
+
+
+def set_logger(custom_logger: Any) -> None:
+    """
+    Set the logger for the entire library.
+
+    Args:
+        custom_logger: The logger object to be used throughout the library.
+    """
+    logger.set_logger(custom_logger)
