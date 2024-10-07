d685c64d27c06c89498bb423bd14b87c736f3d14
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-10-06 19:36:36 -0600

    Add logging handler

--- src/supasaas/_logging/loguru_config.py
+++ src/supasaas/_logging/loguru_config.py
@@ -11,10 +11,26 @@ set_logger(logger)
 
 def stdout_logger() -> None:
     "Set up loguru logger with stdout"
+
+    logger.remove()
     logger.add(
         sys.stdout,
         colorize=True,
-        format="<green>{time}</green> <level>{message}</level>",
+        format="<c><i>{time}</i></c <level>{message}</level>",
+        diagnose=True,
+    )
+
+
+def file_logger() -> None:
+    "Set up loguru logger with file"
+
+    logger.remove()
+    logger.add(
+        "supasaas.log",
+        rotation="1 KB",
+        colorize=False,
+        format="{time:MM/DD HH:mm:ss.ss} - {level} - {message}",
+        diagnose=True,
     )
 
 
@@ -27,7 +43,5 @@ def gcloud_logger() -> None:
 
     logger.remove()
     logger.add(
-        sys.stdout,
-        format=_GCP_LOG_FORMAT,
-        colorize=True,
+        sys.stdout, format=_GCP_LOG_FORMAT, colorize=True, diagnose=False
     )
